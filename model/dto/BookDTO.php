c72d38fe8decca86cb50d274f13a0d5f1bad93c7
Julie-ANANI <65860846+Julie-ANANI@users.noreply.github.com>
2025-06-20 13:33:10 +0200

    feat(model): create DTO book

--- model/dto/BookDTO.php
+++ model/dto/BookDTO.php
@@ -2,9 +2,38 @@
 
 declare(strict_types=1);
 
-namespace App\Model\Dto;
+namespace App\Model\DTO;
 
 class BookDTO
 {
-    
+    public function __construct(
+        public readonly ?int $id,
+        public readonly string $title,
+        public readonly string $author,
+        public readonly ?string $isbn,
+        public readonly int $userId
+    ) {
+    }
+
+    public static function fromEntity(\App\Model\Entity\Book $book): self
+    {
+        return new self(
+            $book->getId(),
+            $book->getTitle(),
+            $book->getAuthor(),
+            $book->getIsbn(),
+            $book->getUserId()
+        );
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'author' => $this->author,
+            'isbn' => $this->isbn,
+            'user_id' => $this->userId
+        ];
+    }
 }
